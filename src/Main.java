095bc8cf4716585bfe80e773514c5b4d477fd3b2
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:05:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+/*
+Girilen 3 sayıyı "küçükten büyüğe" sıralayan programı yazınız.
+ */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n1, n2, n3, temp;
+        Scanner yazici = new Scanner(System.in);
+        System.out.println("Lutfen birinci sayiyi girin: ");
+        n1 = yazici.nextInt();
+        System.out.println("Lutfen ikinci sayiyi girin: ");
+        n2 = yazici.nextInt();
+        System.out.println("Lutfen ucuncu sayiyi girin: ");
+        n3 = yazici.nextInt();
+        if (!(n1 == n2 || n1 == n3 || n2 == n3)) {
+            if (n1 > n3) {
+                temp = n1;
+                n1 = n3;
+                n3 = temp;
+            }
+            if (n1 > n2) {
+                temp = n1;
+                n1 = n2;
+                n2 = temp;
+            }
+            if (n2 > n3) {
+                temp = n2;
+                n2 = n3;
+                n3 = temp;
+            }
+
+            System.out.println("n1: " + n1);
+            System.out.println("n2: " + n2);
+            System.out.println("n3: " + n3);
+
+
+        }
+        else System.out.println("Ayni olan sayilar iceriyor.");
     }
 }
